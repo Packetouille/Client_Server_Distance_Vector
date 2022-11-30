f8b0c5bc417a332df3b879b674651b277b2f9535
alondo1 <alondo1@gmail.com>
2022-11-30 13:54:13 -0500

    Building routing table lists

---
+++ Router1Client.java
@@ -0,0 +1,50 @@
+// To compile and run the code use commands "javac ApplesA1TCPClient.java" followed by "java ApplesA1TCPClient.java" in the command line.
+
+import java.io.*;
+import java.net.*;
+
+class Router1Client{
+    public static void main(String argv[]) throws Exception {
+        String userInput = "";
+        String messageToSend = "";
+        String serverResponse;
+
+        // Create input stream
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        
+        //Create socket, connection to server (hostid and port#). TCP connection setup
+        Socket clientSocket = new Socket("localhost", 11112);
+
+        // Create input (BufferedReader) & output (DataOutputStream) stream attached to socket
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        do{
+            do{
+                System.out.println("Enter the next network IP: ");  
+                userInput = inFromUser.readLine();
+                if(!userInput.equals("-1") && !userInput.equals("exit")){
+                    messageToSend += userInput + " ";
+                    System.out.println("Enter the distance to the network: ");  
+                    messageToSend += userInput = inFromUser.readLine() + " ";
+                    System.out.println("Enter the neighbor: ");  
+                    messageToSend += userInput = inFromUser.readLine() + " ";
+                }
+            } while (!userInput.equals("-1") && !userInput.equals("exit"));
+            if(userInput.equals("-1")){
+                outToServer.writeBytes(messageToSend + '\n');
+            } else{
+                outToServer.writeBytes("exit" + '\n');
+            }
+            if(!userInput.equals("exit")){ 
+                serverResponse = inFromServer.readLine();
+                System.out.println("Response from server: " + serverResponse);
+            }
+
+            messageToSend = "";
+        } while (!userInput.equals("exit"));
+
+        System.out.println("Goodbye");
+        clientSocket.close();
+    }
+}
---
+++ Router2Server.java
@@ -0,0 +1,71 @@
+// To compile and run the code use commands "javac ApplesA1TCPServer.java" followed by "java ApplesTCPServer.java" in the command line.
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class Router2Server{
+    public static List<String> routingTableR2 = new ArrayList<String>();
+    public static void main(String argv[]) throws Exception {
+        String clientString;
+        String userInput;
+        //List<String> r2Table = new ArrayList<String>();
+        List<String> routingTableR1 = new ArrayList<String>();
+        String[] clientStringArray;
+        Scanner userInputScanner = new Scanner(System.in);
+
+        // Create socket for incoming request
+        ServerSocket welcomeSocket = new ServerSocket(11112);
+
+        // Wait for incoming connection request. TCP Connection setup
+        Socket connectionSocket = welcomeSocket.accept();
+
+        // Create input (BufferedReader) & output (DataOutputStream) stream attached to socket
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        
+        do{
+            System.out.println("Enter the next network IP: ");  
+            userInput = userInputScanner.nextLine();
+            if(!userInput.equals("-1") && !userInput.equals("exit")){
+                routingTableR2.add(userInput);
+                System.out.println("Enter the distance to the network: ");  
+                userInput = userInputScanner.nextLine();
+                routingTableR2.add(userInput);
+                System.out.println("Enter the neighbor: ");  
+                userInput = userInputScanner.nextLine();
+                routingTableR2.add(userInput);
+            }
+        } while (!userInput.equals("-1") && !userInput.equals("exit"));
+        
+        System.out.println("routingTableR2 contents: " + routingTableR2);
+
+        while(true){
+        // Keeps server on until user inputs exit, at which point a break from the loop will occur
+            clientString = inFromClient.readLine();
+            System.out.println("Received from Client: " + clientString);
+            
+            clientStringArray = clientString.split(" ");
+
+            for (String element: clientStringArray){
+                routingTableR1.add(element);
+            }
+
+            System.out.println("routingTableR1 contents: " + routingTableR1);
+  //          System.out.println("stringArray = " + stringArray);
+
+            if (!clientString.equals("exit")){
+                outToClient.writeBytes(clientString + '\n');                
+            } else{
+                welcomeSocket.close();
+                System.out.println("Goodbye");
+                break;
+            }
+//            outToClient.writeBytes(clientString);    // '\n' is necessary. Don't know why yet.
+        }
+    }
+
+    public static void runDistanceVectorAlgorithm(){
+        
+    }
+}
